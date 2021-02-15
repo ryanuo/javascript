93c867ce9cf558e1d2c4a8b86ba48b0f1e4fc1f9
R | blog <66169324+w1577791638@users.noreply.github.com>
2021-02-15 09:49:01 +0800

    style: 修改底部浮动效果，适配移动端，取消移动端滑动问题

--- js/style.js
+++ js/style.js
@@ -33,3 +33,16 @@ ibtn.onclick = function () {
         }
     }
 }
+// 页面滑动效果
+var mo=function(e){e.preventDefault();};
+function stop(){
+        document.body.style.overflow='hidden';       
+        document.addEventListener("touchmove",mo,{passive:false});//禁止页面滑动
+}
+//直接默认不让滑动
+stop();
+/***取消滑动限制***/
+function move(){
+        document.body.style.overflow='';//出现滚动条
+        document.removeEventListener("touchmove",mo,{passive:false});       
+}
